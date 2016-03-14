e217f3117cc0d7b1c0dd0152554b64acc35725b4
TJ DeVries <devries.timothyj@gmail.com>
2016-03-13 23:42:19 -0400

    Fix Landscape checks

--- unogame/controller/terminal_controller.py
+++ unogame/controller/terminal_controller.py
@@ -1,10 +1,15 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+Simple terminal controller to play Uno.
+Make a terminal object, and then just run its main function!
+"""
 # Standard Imports
 # from typing import String
 
 # UNO Terminal Controller
+from unogame import my_logger
 from unogame.util.card import Card
 from unogame.util.player import Player
 from unogame.model.model import Model
@@ -14,12 +19,12 @@ winner = None
 
 class TerminalController(object):
     def __init__(self):
-        ''' Create our terminal controller object '''
-        self.model = Model(2)
+        """Create our terminal controller object"""
+        self.model = Model(3)
         self.winner = None
 
     def main(self):
-        ''' Play the game '''
+        """Play the game"""
         while(not self.winner):
             self.play_turn()
 
@@ -30,19 +35,20 @@ class TerminalController(object):
     def display_card(self, card: Card):
         card = [
             '┌─────────┐',
-            '│{}        │'.format(card.rank, ' <2'),
+            '│{:2}       │'.format(card.rank),
             '│         │',
             '│         │',
-            '│    {}    │'.format(card.suit, ' <2'),
+            '│    {:3}  │'.format(card.suit),
             '│         │',
             '│         │',
-            '│       {} │'.format(card.rank, ' >2'),
+            '│      {:2} │'.format(card.rank),
             '└─────────┘',
         ]
 
         return card
 
     def display_hand(self, player: Player):
+        """Displays the hand in a nice string."""
         # TODO: Kind of ugly hack. Just made with empty strings and a new line at the end
         to_display = ['', '', '', '', '', '', '', '', '', '']
         for card in player.hand:
@@ -53,7 +59,7 @@ class TerminalController(object):
         return '\n'.join(to_display)
 
     def play_turn(self):
-        ''' Perform one turn in the game '''
+        """Perform one turn in the game"""
         player_status = '========================================\n'
         for player in self.model.players:
             player_status += '{} has {} cards\n'.format(
@@ -71,51 +77,31 @@ class TerminalController(object):
         # Have the player make his/her move and then
         #   Handle the result once it is completed
         effect = player.play()
-        # Move the current player -> next player
-        self.model.increment_player()
 
         if effect:
             if 'skip' in effect.keys():
+                my_logger.debug('Player {0} played a `{1}`'.format(player.name, 'SKIP'))
                 self.model.skip()
 
             if 'reverse' in effect.keys():
+                my_logger.debug('Player {0} played a `{1}`'.format(player.name, 'REVERSE'))
                 self.model.reverse()
 
             if 'draw' in effect.keys():
-                for new_card in range(effect['draw']):
-                    self.model.current_player.draw_card()
+                my_logger.debug('Player {0} played a `{1}`'.format(player.name, 'DRAW'))
+                for _ in range(effect['draw']):
+                    self.model.next_player.draw_card()
 
             if 'wild' in effect.keys():
                 # TODO
                 pass
 
+        # Move the current player -> next player
+        self.model.increment_player()
+
         if self.model.game_over():
             self.winner = player
 
-'''
-def check_for_neutral():
-    print('CHECKING NEUTRAL')
-
-
-def has_winner():
-    if Player1.hand.is_empty() or AIPlayer.hand.is_empty():
-        print('THE WINNER IS: ', get_winner())
-        return True
-    return False
-
-
-def get_winner():
-    if Player1.hand.is_empty():
-        return Player1.name
-    elif AIPlayer.hand.is_empty():
-        return AIPlayer.name
-
-
-def check_for_winner():
-    if Player1.hand.is_empty()or AIPlayer.hand.is_empty():
-        return True
-    return False
-'''
 
 if __name__ == '__main__':
     t = TerminalController()
